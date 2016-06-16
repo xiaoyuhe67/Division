5078aa0261330d5adf4c5b58ae566ab935fc25eb
xiaoyuhe67 <xiaoyuhe67@github.com>
2016-06-16 15:14:59 -0400

    Division Exception

---
+++ src/Division.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+class Division {
+  public static void main(String[] args) {
+ 
+	  try
+	  {
+		  	int a, b, result;
+
+		  	Scanner input = new Scanner(System.in);
+		  	System.out.println("Input two integers");
+		  	
+		  	a = input.nextInt();
+		  	b = input.nextInt();
+		  	
+		  	result = a / b;
+		  	
+		  	System.out.println("Result = " + result);
+	  }catch( ArithmeticException e )
+	  {
+		  System.out.println("Do not enter 0 for the second integer");
+	  }
+ 
+      
+  }
+}
